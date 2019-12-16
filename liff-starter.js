40d728f5722477ace46b25c4bad5cbd4247f5538
Valdy Rizki Djuliansyah <51867202+valdyrizki@users.noreply.github.com>
2019-12-16 16:05:04 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const useNodeJS = true;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1653656282-a7qK5Az0";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
